ac5dcaf7d0426061986a4582e0a5fac63b6315cc
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-21 05:12:49 +0200

    +

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 // Находим элементы на странице
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
 
-// Функции
-
+form.addEventListener('submit', function(event) {
 	// Отменяем отправку формы
+	event.preventDefault();
 
 	// Достаем текст задачи из поля ввода
+	const taskText = taskInput.value;
+
+	// Формируем разметку для новой задачи
+	const taskHTML = `
+		<li class="list-group-item d-flex justify-content-between task-item">
+			<span class="task-title">${taskText}</span>
+			<div class="task-item__buttons">
+				<button type="button" data-action="done" class="btn-action">
+					<img src="./img/tick.svg" alt="Done" width="18" height="18">
+				</button>
+				<button type="button" data-action="delete" class="btn-action">
+					<img src="./img/cross.svg" alt="Done" width="18" height="18">
+				</button>
+			</div>
+		</li>
+	`;
+
+	// Добавляем задачу на страницу
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
+});
+
+// Функции
 
 	// Описываем задачу в виде объекта
 	
