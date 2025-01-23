e9165fd4dd3ab2d63052d91125875af3a876a354
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-23 14:12:52 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 // Находим элементы на странице
+
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
+
+let tasks = [];
 
 form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
 
 // Функции
+
 function addTask(event) {
 	// Отменяем отправку формы
 	event.preventDefault();
@@ -16,10 +20,23 @@ function addTask(event) {
 	// Достаем текст задачи из поля ввода
 	const taskText = taskInput.value;
 
+	// Описываем задачу в виде объекта
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		done: false,
+	};
+
+	// Добавляем задачу в массив с задачами
+	tasks.push(newTask);
+
+	// Формируем CSS класс
+	const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
+
 	// Формируем разметку для новой задачи
 	const taskHTML = `
-		<li class="list-group-item d-flex justify-content-between task-item">
-			<span class="task-title">${taskText}</span>
+		<li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+			<span class="${cssClass}">${newTask.text}</span>
 			<div class="task-item__buttons">
 				<button type="button" data-action="done" class="btn-action">
 					<img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -39,28 +56,28 @@ function addTask(event) {
 	taskInput.focus();
 
 	// Проверка. Если в списке задач более 1 - го элемента, скрываем блок "Список дел пуст"
-	if(tasksList.children.length > 1) {
+	if (tasksList.children.length > 1) {
 		emptyList.classList.add('none');
 	}
 }
 
 function deleteTask(event) {
 	// Проверяем если клик был НЕ по кнопке "удалить задачу"
-	if(event.target.dataset.action !== 'delete') return;
+	if (event.target.dataset.action !== 'delete') return;
 
 	// Если клик был по кнопке "удалить задачу"
 	const parentNode = event.target.closest('li');
 	parentNode.remove();
 
 	// Проверка. Если в списке задач 1-ин элемент, показываем блок "Список дел пуст"
-	if(tasksList.children.length === 1) {
+	if (tasksList.children.length === 1) {
 		emptyList.classList.remove('none');
 	}
 }
 
 function doneTask(event) {
 	// Проверяем что клик был НЕ по кнопке "задача выполнена"
-	if(event.target.dataset.action !== 'done') return;
+	if (event.target.dataset.action !== 'done') return;
 
 	// Если клик был по кнопке "задача выполнена"
 	const parentNode = event.target.closest('.list-group-item');
@@ -68,34 +85,32 @@ function doneTask(event) {
 	taskTitle.classList.toggle('task-title--done');
 }
 
-	// Описываем задачу в виде объекта
-	
-	// Добавляем задачу в массив с задачами
 
-	// Сохраняем список задач в хранилище браузера localStorage
 
-	// Рендерим задачу на странице
+// Сохраняем список задач в хранилище браузера localStorage
 
-	// Очищаем поле ввода и возвращаем на него фокус
+// Рендерим задачу на странице
 
-	// Проверяем если клик был НЕ по кнопке "удалить задачу"
+// Очищаем поле ввода и возвращаем на него фокус
 
-	// Определяем ID задачи
+// Проверяем если клик был НЕ по кнопке "удалить задачу"
 
-	// Удаляем задча через фильтрацию массива
+// Определяем ID задачи
 
-	// Сохраняем список задач в хранилище браузера localStorage
+// Удаляем задча через фильтрацию массива
 
-	// Удаляем задачу из разметки
+// Сохраняем список задач в хранилище браузера localStorage
 
-	// Проверяем что клик был НЕ по кнопке "задача выполнена"
+// Удаляем задачу из разметки
 
-	// Определяем ID задачи
+// Проверяем что клик был НЕ по кнопке "задача выполнена"
 
-	// Сохраняем список задач в хранилище браузера localStorage
+// Определяем ID задачи
 
-	// Формируем CSS класс
+// Сохраняем список задач в хранилище браузера localStorage
 
-	// Формируем разметку для новой задачи
+// Формируем CSS класс
 
-	// Добавляем задачу на страницу
+// Формируем разметку для новой задачи
+
+// Добавляем задачу на страницу
