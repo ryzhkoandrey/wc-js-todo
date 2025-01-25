6e117a27fe2c154cd13bac521cd381bdb1d97345
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-25 06:20:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,34 @@ const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 
 let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+	tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+tasks.forEach(function (task) {
+	// Формируем CSS класс
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+	// Формируем разметку для новой задачи
+	const taskHTML = `
+		<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+			<span class="${cssClass}">${task.text}</span>
+			<div class="task-item__buttons">
+				<button type="button" data-action="done" class="btn-action">
+					<img src="./img/tick.svg" alt="Done" width="18" height="18">
+				</button>
+				<button type="button" data-action="delete" class="btn-action">
+					<img src="./img/cross.svg" alt="Done" width="18" height="18">
+				</button>
+			</div>
+		</li>
+	`;
+
+	// Добавляем задачу на страницу
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
+});
+
 checkEmptyList();
 
 form.addEventListener('submit', addTask);
